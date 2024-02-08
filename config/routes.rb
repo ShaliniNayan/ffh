d7c494bbc0ecb55a7a90366c61d5e0e91daedecb
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-02-08 13:36:57 +0530

    Update Admin to root and authenticate

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "home#index"
+
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
 end
