0495335815b3b19e2030e9f5941fb5e61bac47fd
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-03-06 22:48:10 +0530

    Migrate data base

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_03_152042) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_06_170121) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,17 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_03_152042) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "admin_stocks", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.integer "category_id", null: false
+    t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_admin_stocks_on_category_id"
   end
 
   create_table "admins", force: :cascade do |t|
@@ -71,5 +82,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_03_152042) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "admin_stocks", "categories"
   add_foreign_key "products", "categories"
 end
